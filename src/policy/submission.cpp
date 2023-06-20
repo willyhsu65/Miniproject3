d5cbb2648dde2b2d7988b8ee6bf4b93a20978aba
willyhsu65 <12mg105g@gmail.com>
2023-06-20 21:28:16 +0800

    submission added

---
+++ src/policy/submission.cpp
@@ -0,0 +1,69 @@
+#include <cstdlib>
+#include <algorithm>
+#include <climits>
+
+#include "../state/state.hpp"
+#include "./alphabeta.hpp"
+
+int alpha_value = INT_MIN;
+int beta_value = INT_MAX;
+Move Alphabeta::getmove(State *node,int depth)
+{
+    alpha_value = INT_MIN;
+    beta_value = INT_MAX;
+    if (node->legal_actions.empty())
+        node->get_legal_actions();
+    int heuristic_value = INT_MIN;
+    auto actions = node->legal_actions;
+    Move next;
+    for (auto move : actions)
+    {
+        int heuristic = alphabeta(node->next_state(move),depth - 1,false,alpha_value,beta_value);
+        if (heuristic_value <= heuristic)
+        {
+            heuristic_value = heuristic;
+            next = move;
+        }
+    }
+    return next;
+}
+int alphabeta(State *node,int depth,bool maximizing_player,int alpha,int beta)
+{
+    if (depth == 0)
+    {
+        if (maximizing_player)
+            return node->evaluate()*(-1);
+        else
+            return node->evaluate();
+    }
+
+    if (maximizing_player)
+    {
+        int value = INT_MIN;
+        node->get_legal_actions();
+        auto actions = node->legal_actions;
+        for (auto move : actions)
+        {
+            value = std::max(value,alphabeta(node->next_state(move),depth - 1,false,alpha,beta));
+            alpha = std::max(value,alpha);
+            if (alpha >= beta)
+                break;
+        }
+
+        return value;
+    }
+    else
+    {
+        int value = INT_MAX;
+        node->get_legal_actions();
+        auto actions = node->legal_actions;
+        for (auto move : actions)
+        {
+            value = std::min(value,alphabeta(node->next_state(move),depth - 1,true,alpha,beta));
+            beta = std::min(beta,value);
+            if (beta <= alpha)
+                break;
+        }
+        return value;
+    }
+}
